96af5a2b4916c669c19f964ef9549041bfe93b68
maxime-antoine <maxime.antoine001@gmail.com>
2025-06-04 12:31:31 +0200

    flying slime

---
+++ src/graphics/shape/FlyingSlimeShape.java
@@ -0,0 +1,28 @@
+package graphics.shape;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import tools.PathManager;
+
+public final class FlyingSlimeShape extends Shape {
+
+    private static final BufferedImage LEFT_MASK;
+    private static final BufferedImage RIGHT_MASK;
+    private static final BufferedImage TOP_MASK;
+
+    static {
+        try {
+            String maskPath = PathManager.MASK_PATH + "flyingSlime/";
+            LEFT_MASK  = loadMask(maskPath + "mask-left.png");
+            RIGHT_MASK = loadMask(maskPath + "mask-right.png");
+            TOP_MASK   = loadMask(maskPath + "mask-top.png");
+        } catch (IOException ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public FlyingSlimeShape() {
+        super(LEFT_MASK, RIGHT_MASK, TOP_MASK, true);
+    }
+}
